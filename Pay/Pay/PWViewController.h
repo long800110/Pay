6ee55fbad5e60aa9a98fe771c8dc24923c9c268a
Lin Chen <linchen.1987@foxmail.com>
2015-06-29 23:04:06 +0800

    add Password Page and Main Page

---
+++ Pay/Pay/PWViewController.h
@@ -0,0 +1,13 @@
+//
+//  PWViewController.h
+//  Pay
+//
+//  Created by Lin Chen on 15/6/29.
+//  Copyright (c) 2015年 PoC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWViewController : UIViewController
+
+@end
---
+++ Pay/Pay/PWViewController.m
@@ -0,0 +1,37 @@
+//
+//  PWViewController.m
+//  Pay
+//
+//  Created by Lin Chen on 15/6/29.
+//  Copyright (c) 2015年 PoC. All rights reserved.
+//
+
+#import "PWViewController.h"
+
+@interface PWViewController ()
+
+@end
+
+@implementation PWViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
